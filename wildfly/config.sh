feee78802e0245200893581744a025f42770f887
Michał Zając <michal.t.zajac@gmail.com>
2015-09-13 14:04:42 +0200

    Wildfly 9.0.1

--- wildfly/config.sh
+++ wildfly/config.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-/opt/wildfly-8.2.0.Final/bin/standalone.sh --server-config=standalone-full.xml &
-PID=$!
+WILDFLY_HOME=/opt/wildfly-9.0.1.Final
+
 sleep 10
-echo "module add --name=org.postgresql --resources=/tmp/postgresql-9.3-1102.jdbc41.jar --dependencies=javax.api,javax.transaction.api" | /opt/wildfly-8.2.0.Final/bin/jboss-cli.sh --connect
-echo "/subsystem=datasources/jdbc-driver=postgresql:add(driver-name=postgresql,driver-class-name=org.postgresql.Driver,driver-module-name=org.postgresql,driver-xa-datasource-class-name=org.postgresql.xa.PGXADataSource)" | /opt/wildfly-8.2.0.Final/bin/jboss-cli.sh --connect
-echo "KILLING Wildfly"
-kill $PID
+echo "module add --name=org.postgresql --resources=/tmp/postgresql-9.4-1202.jdbc42.jar --dependencies=javax.api,javax.transaction.api" | $WILDFLY_HOME/bin/jboss-cli.sh --connect
+connectecho "/subsystem=datasources/jdbc-driver=postgresql:add(driver-name=postgresql,driver-class-name=org.postgresql.Driver,driver-module-name=org.postgresql,driver-xa-datasource-class-name=org.postgresql.xa.PGXADataSource)" | $WILDFLY_HOME/bin/jboss-cli.sh --connect
+
+
+if [[ ! -z $ADMIN_PASSWORD ]]; then
+    echo "Adding admin user"
+fi
+
---
+++ wildfly/wildfly.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+WILDFLY_HOME=/opt/wildfly-9.0.1.Final
+#export JAVA_OPTS="${JAVA_OPTS} -Djboss.bind.address=0.0.0.0"
+
+$WILDFLY_HOME/bin/standalone.sh -b=0.0.0.0 -bmanagement=0.0.0.0 -server-config=standalone-full.xml > /var/log/wildfly.log 2>&1 &
+PID=$!
+
+ls $WILDFLY_HOME/configured > /dev/null 2>&1
+if [ $? != 0 ]; then
+    echo "Configuring wildfly"
+
+    if [ ! -z $ADMIN_PASSWORD ]; then
+        echo "Adding user admin"
+        $WILDFLY_HOME/bin/add-user.sh -u admin -s -p $ADMIN_PASSWORD
+    fi
+
+
+    netstat -tln | grep :9990 > /dev/null 2>&1
+    while [ $? != 0 ]; do
+        echo "not running, waiting one second"
+        sleep 1;
+        netstat -tln | grep :9990 > /dev/null 2>&1
+    done
+
+    echo "module add --name=org.postgresql --resources=/tmp/postgresql-9.4-1202.jdbc42.jar --dependencies=javax.api,javax.transaction.api" | $WILDFLY_HOME/bin/jboss-cli.sh --connect
+    echo "/subsystem=datasources/jdbc-driver=postgresql:add(driver-name=postgresql,driver-class-name=org.postgresql.Driver,driver-module-name=org.postgresql,driver-xa-datasource-class-name=org.postgresql.xa.PGXADataSource)" | $WILDFLY_HOME/bin/jboss-cli.sh --connect
+
+    touch $WILDFLY_HOME/configured
+    echo "Wildfly configured"
+else
+    echo "Already confiugred"
+
+fi
+
+#exec $WILDFLY_HOME/bin/standalone.sh --server-config=standalone-full.xml
+
+wait $PID
